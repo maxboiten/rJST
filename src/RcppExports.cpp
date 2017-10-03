d24153d9cbfd2b82683acd44bc5cbf1f0e77daaa
Max Boiten <max_boiten@live.nl>
2017-10-03 11:12:07 +0200

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,12 +7,12 @@
 using namespace Rcpp;
 
 // jstcpp
-int jstcpp(string model_status_str, Rcpp::List corpus, Rcpp::List sentiLexList, int numSentiLabs, int numTopics, int numiters, int updateParaStep, double alpha_, double beta_, double gamma_);
+int jstcpp(std::string model_status_str, Rcpp::List corpus, Rcpp::List sentiLexList, int numSentiLabs, int numTopics, int numiters, int updateParaStep, double alpha_, double beta_, double gamma_);
 RcppExport SEXP _rJST_jstcpp(SEXP model_status_strSEXP, SEXP corpusSEXP, SEXP sentiLexListSEXP, SEXP numSentiLabsSEXP, SEXP numTopicsSEXP, SEXP numitersSEXP, SEXP updateParaStepSEXP, SEXP alpha_SEXP, SEXP beta_SEXP, SEXP gamma_SEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< string >::type model_status_str(model_status_strSEXP);
+    Rcpp::traits::input_parameter< std::string >::type model_status_str(model_status_strSEXP);
     Rcpp::traits::input_parameter< Rcpp::List >::type corpus(corpusSEXP);
     Rcpp::traits::input_parameter< Rcpp::List >::type sentiLexList(sentiLexListSEXP);
     Rcpp::traits::input_parameter< int >::type numSentiLabs(numSentiLabsSEXP);
