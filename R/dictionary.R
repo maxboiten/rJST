a670996e354bc7bcbacda5ab5b3431ec4fdaa89e
Max Boiten <max_boiten@live.nl>
2017-10-11 10:10:36 +0200

    change wordstem dict and roxygenise

--- R/dictionary.R
+++ R/dictionary.R
@@ -33,7 +33,7 @@ paradigm <- function() {
 #' @return A quanteda dictionary2 object with all elements stemmed
 #' 
 #' @export
-wordstem.dictionary2 <- function(dict) {
+wordstemdict <- function(dict) {
   sentiments <- names(dict)
   dictList <- as.list(dict)
   for (name in sentiments) {
