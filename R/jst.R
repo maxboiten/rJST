a8d889dae6e1618a5dc51c0a9263a0c0e71b9c44
Max Boiten <max_boiten@live.nl>
2019-12-09 15:26:40 +0100

    Fix default argument dictionary
    From list() to NULL with appropriate checks

--- R/jst.R
+++ R/jst.R
@@ -2,9 +2,9 @@
 #' @importFrom methods new
 
 #' @title JST results object
-#' 
+#'
 #' @description Contains estimated Joint Sentiment Topic model
-#' 
+#'
 #' @slot pi Document-level sentiment estimates
 #' @slot theta Document-level sentitopic estimates
 #' @slot phi Word-level sentitopic estimates
@@ -12,8 +12,8 @@
 #' @slot numTopics Number of topics
 #' @slot numSentiments Number of sentiment categories
 #' @slot docvars Document-level metadata from the quanteda object used as input
-JST.result <- setClass('JST.result',representation(pi = "data.frame", 
-                                     theta = "data.frame", 
+JST.result <- setClass('JST.result',representation(pi = "data.frame",
+                                     theta = "data.frame",
                                      phi = "data.frame",
                                      phi.termScores = "data.frame",
                                      numTopics = "numeric",
@@ -21,28 +21,28 @@ JST.result <- setClass('JST.result',representation(pi = "data.frame",
                                      docvars = "data.frame"))
 
 #' Check if an object is a JST.result object
-#' 
+#'
 #' @param x object
-#' 
+#'
 #' @return Boolean. True if x is a JST.result object.
-#' 
+#'
 #' @export
 is.JST.result <- function(x) {
   return(inherits(x,'JST.result'))
 }
 
 #' Run a Joint Sentiment Topic model
-#' 
+#'
 #' Estimates a joint sentiment topic model using a Gibbs sampler, see Details for model description.
-#' 
+#'
 #' Basic model description:
-#' 
-#' Lin, C. and He, Y., 2009, November. Joint sentiment/topic model for sentiment analysis. In 
+#'
+#' Lin, C. and He, Y., 2009, November. Joint sentiment/topic model for sentiment analysis. In
 #' Proceedings of the 18th ACM conference on Information and knowledge management (pp. 375-384). ACM.
-#' 
+#'
 #' Weak supervision adopted from:
-#' 
-#' Lin, C., He, Y., Everson, R. and Ruger, S., 2012. Weakly supervised joint sentiment-topic 
+#'
+#' Lin, C., He, Y., Everson, R. and Ruger, S., 2012. Weakly supervised joint sentiment-topic
 #' detection from text. IEEE Transactions on Knowledge and Data engineering, 24(6), pp.1134-1145.
 #'
 #' @param dfm A quanteda dfm object
@@ -62,14 +62,15 @@ is.JST.result <- function(x) {
 #' to \code{TRUE}, the neutral sentiment category will be omitted. The variable is irrelevant if no
 #' dictionary is used. Defaults to \code{FALSE}.
 #' @return A JST.result object containing a data.frame for each estimated parameter
-#' 
-#' @examples 
-#' model <- jst(quanteda::dfm(quanteda::data_corpus_irishbudget2010), 
-#'              paradigm(), 
-#'              numTopics = 5, 
+#'
+#' @examples
+#' model <- jst(quanteda::dfm(quanteda::data_corpus_irishbudget2010),
+#'              paradigm(),
+#'              numTopics = 5,
 #'              numIters = 150)
 #' @export
-jst <- function(dfm,sentiLexInput=list(),
+jst <- function(dfm,
+                sentiLexInput=NULL,
                 numSentiLabs = 3,
                 numTopics = 10,
                 numIters = 3,
@@ -78,70 +79,71 @@ jst <- function(dfm,sentiLexInput=list(),
                 beta = -1,
                 gamma = -1,
                 excludeNeutral = FALSE) {
-  
+
   if (!any(class(dfm) == 'dfm')) {
     stop('Please input a sparse quanteda dfm object as data.')
   }
-  
+
   sentiWords <- integer()
   sentimentCategory <- integer()
-  
-  if(quanteda::is.dictionary(sentiLexInput)) {
-    numSentiLabs_Lex <- length(sentiLexInput)
-    numSentiLabs <- numSentiLabs_Lex + 1 - excludeNeutral
-    
-    size <- 1
-    for (i in c(1:numSentiLabs_Lex)) {
-      for (word in sentiLexInput[[i]]) {
-        if(word %in% quanteda::featnames(dfm)) {
-          sentiWords[size] <- as.integer(match(word,quanteda::featnames(dfm))-1) #-1 for C++ index
-          sentimentCategory[size] <- as.integer(i-excludeNeutral)
-          size <- size + 1
+
+  if (!is.null(sentiLexInput)){
+    if(quanteda::is.dictionary(sentiLexInput)) {
+      numSentiLabs_Lex <- length(sentiLexInput)
+      numSentiLabs <- numSentiLabs_Lex + 1 - excludeNeutral
+
+      size <- 1
+      for (i in c(1:numSentiLabs_Lex)) {
+        for (word in sentiLexInput[[i]]) {
+          if(word %in% quanteda::featnames(dfm)) {
+            sentiWords[size] <- as.integer(match(word,quanteda::featnames(dfm))-1) #-1 for C++ index
+            sentimentCategory[size] <- as.integer(i-excludeNeutral)
+            size <- size + 1
+          }
         }
       }
+    } else {
+      stop('The input lexicon needs to be a quanteda dictionary object.')
     }
-    
-  } else {
-    stop('The input lexicon needs to be a quanteda dictionary object.')
   }
-  
+
   res <- jstcpp(dfm,sentiWords,sentimentCategory,numSentiLabs, numTopics, numIters, updateParaStep, alpha,beta,gamma)
-  
+
   if(length(res) == 0) {return(".")}
-  
+
   #prepare doc sentiment distribution data.frame
   docIDs <- attr(dfm,'Dimnames')$docs
-  
+
   pi <- as.data.frame(res$pi)
   pi <- as.data.frame(t(pi))
-  
+
   pi.names = character(numSentiLabs)
   for (i in c(1:numSentiLabs)) {
     pi.names[i] <- paste("sent",i,sep="")
   }
   names(pi) <- pi.names
   rownames(pi) <- docIDs
-  
+
   #prepare doc sentiment/topic distribution data.frame
   theta <- as.data.frame(res$theta)
-  
+
   theta.names <- character(numTopics)
-  
+
   theta.names = character(numSentiLabs*numTopics)
   for (i in c(1:numSentiLabs)) {
     for (j in c(1:numTopics)) {
       theta.names[j+numTopics*(i-1)] <- paste("topic",j,"sent",i,sep="")
     }
   }
-  
+
   names(theta) <- theta.names
-  
+
   theta <- data.frame(docIDs,theta,row.names=NULL)
-  
+
   #prepare word topic/sentiment distribtuion data.frame
   phi <- as.data.frame(res$phi)
   phi.termScores <- as.data.frame(res$phi.termScores)
-  
+
   phi.names = character(numSentiLabs*numTopics)
   for (i in c(1:numSentiLabs)) {
     for (j in c(1:numTopics)) {
@@ -152,7 +154,7 @@ jst <- function(dfm,sentiLexInput=list(),
   names(phi.termScores) <- phi.names
   rownames(phi) <- attr(dfm,'Dimnames')$features
   rownames(phi.termScores) <- attr(dfm,'Dimnames')$features
-  
+
   return(JST.result(pi = pi,
              theta = theta,
              phi = phi,
@@ -167,15 +169,15 @@ jst <- function(dfm,sentiLexInput=list(),
 setMethod('topNwords', c('JST.result','numeric','numeric','numeric'),
           function(x,N,topic,sentiment) {
             colname <- paste('topic',topic,'sent',sentiment,sep='')
-            
+
             column <- sapply(x@phi[colname],as.numeric)
-            
+
             res <- rownames(x@phi)[topNwordSeeds(column,N)]
 
             res <- as.data.frame(res,stringsAsFactors = FALSE)
 
             names(res) <- colname
-            
+
             return(res)
           })
 
@@ -184,13 +186,13 @@ setMethod('topNwords', c('JST.result','numeric','numeric','numeric'),
 setMethod('topNwords', c('JST.result','numeric'),
           function(x,N) {
             res <- as.data.frame(matrix(ncol = 0, nrow = N))
-            
+
             for (topic in c(1:x@numTopics)) {
               for (sentiment in c(1:x@numSentiments)) {
                 res <- cbind(res,topNwords(x,N,topic,sentiment))
               }
             }
-            
+
             return(res)
           })
 
